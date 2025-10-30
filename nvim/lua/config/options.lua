3a318c2a1cb11ef21bb657ae1ae56b3a80779cd2
lbesnard <laurent.besnard@utas.edu.au>
2025-10-30 11:03:07 +1100

    wip

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,4 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.clipboard = "unnamedplus" --https://www.reddit.com/r/neovim/comments/158uhq0/how_to_copy_text_in_neovim_to_system_clipboard/
