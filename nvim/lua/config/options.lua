783ea9764d567ade31a2694b4609cca90c919a1b
lbesnard <laurent.besnard@utas.edu.au>
2026-04-16 11:51:40 +1000

    Fix: add clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.clipboard = "unnamedplus" --https://www.reddit.com/r/neovim/comments/158uhq0/how_to_copy_text_in_neovim_to_system_clipboard/
+
+-- Only use OSC 52 if we are in an SSH session
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
